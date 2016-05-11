313971ca5a66e4b8ee40c1f9489c01af15f7ef09
Gilsonifpe <Gilson.oliveira96@outlook.com>
2016-05-10 21:17:12 -0300

    Calculadora

---
+++ src/Calc2.java
@@ -0,0 +1,17 @@
+
+public class Calc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Calculadora  c = new Calculadora();
+		System.out.println(c.Dividir(8, 4)); 
+		System.out.println(c.somar(5, 5));
+		System.out.println(c.multiplicar(3, 3));
+		System.out.println(c.subtrair(4, 2));
+		
+		
+	}
+
+}
---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+
+public class Calculadora {
+
+	public double somar(double param1,double param2){
+		return param1 + param2;
+	}
+	
+	public double subtrair(double param1,double param2){
+		return param1 - param2;
+	}
+	
+	public double multiplicar(double param1,double param2){
+		return param1 * param2;
+	}
+	
+	public double Dividir(double param1,double param2){
+		return param1 / param2;
+	}
+		
+	
+	
+}
