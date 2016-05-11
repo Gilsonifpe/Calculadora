55a00a05119e993f9d7a19157112c2e24ab6dcd7
Gilsonifpe <Gilson.oliveira96@outlook.com>
2016-05-10 21:25:20 -0300

    Novo

--- src/Calc2.java
+++ src/Calc2.java
@@ -4,7 +4,7 @@ public class Calc2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+		//exibir data 11/05/2016
 		Calculadora  c = new Calculadora();
 		System.out.println(c.Dividir(8, 4)); 
 		System.out.println(c.somar(5, 5));
--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,20 +1,20 @@
 
 public class Calculadora {
-
+		//metodos
 	public double somar(double param1,double param2){
-		return param1 + param2;
-	}
-	
-	public double subtrair(double param1,double param2){
-		return param1 - param2;
-	}
-	
-	public double multiplicar(double param1,double param2){
 		return param1 * param2;
 	}
 	
-	public double Dividir(double param1,double param2){
+	public double subtrair(double param1,double param2){
+		return param1 + param2;
+	}
+	
+	public double multiplicar(double param1,double param2){
 		return param1 / param2;
+	}
+	
+	public double Dividir(double param1,double param2){
+		return param1 - param2;
 	}
 		
 	
